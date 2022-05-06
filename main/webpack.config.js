1b6f272e9a1de942c9cd42d03bf1505cf3f508a1
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2022-05-06 16:57:58 +0800

    fix: error

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
-const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 const path = require('path');
 
 module.exports = {
